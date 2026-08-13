0efc77815f85a39479a9cb2c47cf8d63f1224cb1
lidzytapia079-hub <lidzytapia079@gmail.com>
2026-08-13 17:24:20 +0000

    py complete

---
+++ story.py
@@ -0,0 +1,17 @@
+def main():
+    planet=input("Planet: ")
+
+    # Separation
+    print("Hello", planet)
+
+    # Ending
+    print("Hello", end=" ")
+    print(planet)
+
+    #Concatenation
+    print("Hello " + planet)
+    #Formatted String
+    print(f"Hello {planet}")
+
+if __name__ == "__main__":
+    main()
