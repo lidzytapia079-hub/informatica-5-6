82b0d0315232c4a96735df6d2a78a6416132787c
lidzytapia079-hub <lidzytapia079@gmail.com>
2026-08-17 19:39:52 +0000

    77

---
+++ rectangle.py
@@ -0,0 +1,12 @@
+def main():
+    width  = int(input(("Enter the width of the rectangle: ")))
+    print("o"*width)
+    print("o"*width)
+    print("o"*width)
+    print("o"*width)
+    print("o"*width)
+    p= (5 * 2)+(width * 2)
+    print("perimetrer:",p)
+
+if __name__ == "__main__":
+    main()
