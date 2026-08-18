31317879a7d4793a09ce84fe7e26c513300f9bdf
lidzytapia079-hub <lidzytapia079@gmail.com>
2026-08-18 16:40:24 +0000

    currency

---
+++ currency.py
@@ -0,0 +1,8 @@
+def main():
+    colombian = print(input("how many colombian pesos do you have left?"))
+    peruvian=print(input("how many peruvian soles do you have left?"))
+    brazilian=print(input("how many brazilian reais do you have left?"))
+
+    print(f{usd})
+    print(f{mxn})
+
