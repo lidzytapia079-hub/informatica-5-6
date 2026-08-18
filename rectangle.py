fba62986eb22db607585d2b72618e7369f8baa89
lidzytapia079-hub <lidzytapia079@gmail.com>
2026-08-18 16:24:59 +0000

    recangle

--- rectangle.py
+++ rectangle.py
@@ -7,6 +7,13 @@ def main():
     print("o"*width)
     p= (5 * 2)+(width * 2)
     print("perimetrer:",p)
+    AREA=width *5
+    print(f"This is the rectangle: {AREA}")
+    Diagonal=(width**2+5**2)**0.5
+    print(f"This is the rectanle´s diagonal:{Diagonal}")
+    perimeter=(width+5)*2
+    print(f"This is the perimeter;{perimeter}")
+
 
 if __name__ == "__main__":
     main()
