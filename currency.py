0922d5524ebb101f78bd64d0798147dfe0137e65
lidzytapia079-hub <lidzytapia079@gmail.com>
2026-08-20 17:13:13 +0000

    currencyfinished

--- currency.py
+++ currency.py
@@ -1,15 +1,20 @@
 def main():
-    colombian =float(input("how many colombian pesos do you have left?"))
-    peruvian=float(input("how many peruvian soles do you have left?"))
-    brazilian=float(input("how many brazilian reais do you have left?"))
+    p =float(input("how many colombian pesos do you have left?"))
+    s=float(input("how many peruvian soles do you have left?"))
+    r=float(input("how many brazilian reais do you have left?"))
+
     mxn = (p* 0.0054) + (s* 5.07)+(r*3.28)
     usd = mxn / 17.06
-    print ("USD" )
+
+    print("USD:", round(usd, 2))
+    print("MXN:", round(mxn, 2))
+
+if __name__=="__main__":
+    main()
 
 
 
-    print(f{usd})
-    print(f{mxn})
 
 
-if__
+
+
