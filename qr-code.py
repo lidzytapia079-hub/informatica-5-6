90cc4f877ef1b4b02f7872747ecca82c732299c8
lidzytapia079-hub <lidzytapia079@gmail.com>
2026-08-20 20:15:27 +0000

    qr

---
+++ qr-code.py
@@ -0,0 +1,19 @@
+import qrcode
+
+def main():
+    song ="https://www.youtube.com/watch?v=q7F83UQXf-Q&list=RDq7F83UQXf-Q&start_radio=1"
+    qr = qrcode.QRCode(version= 1,box_size= 5, border = 5)
+    qr.add_data(song)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color="blue", back_color="white")
+    img.save("my-qrcode.png")
+
+
+if __name__=="_-main__":
+    main()
+
+
+
+
+
