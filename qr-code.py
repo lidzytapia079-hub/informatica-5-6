a47bff6cf3722b6e2a0e50e24c715c8fed1804ec
lidzytapia079-hub <lidzytapia079@gmail.com>
2026-08-20 20:21:30 +0000

    qr

--- qr-code.py
+++ qr-code.py
@@ -2,15 +2,19 @@ import qrcode
 
 def main():
     song ="https://www.youtube.com/watch?v=q7F83UQXf-Q&list=RDq7F83UQXf-Q&start_radio=1"
-    qr = qrcode.QRCode(version= 1,box_size= 5, border = 5)
+    qr = qrcode.QRCode(version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
     qr.add_data(song)
     qr.make(fit=True)
 
-    img = qr.make_image(fill_color="blue", back_color="white")
+    img = qr.make_image(fill_color="black", back_color="white")
     img.save("my-qrcode.png")
 
 
-if __name__=="_-main__":
+if __name__=="__main__":
     main()
 
 
