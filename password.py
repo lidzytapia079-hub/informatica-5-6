a76074cc11f567ba6ceb56db32cc898da198d120
lidzytapia079-hub <lidzytapia079@gmail.com>
2026-08-24 19:37:01 +0000

    PASSWORDC

---
+++ password.py
@@ -0,0 +1,8 @@
+def main():
+    password= "siMUlaTIoN"
+    attempt= input("guess the password:")
+    if attempt == password:
+        print("correct password.")
+    print("end of the program.")
+if __name__=="__main__":
+    main()
