7be025f998b64386015e0a5700cdec2006ac1f3b
lidzytapia079-hub <lidzytapia079@gmail.com>
2026-08-26 20:03:26 +0000

    ex1

---
+++ ifexercises.py
@@ -0,0 +1,10 @@
+def main():
+    interger = int(input("put an int number:"))
+    if interger<0:
+        print("the number multiplied by -1.")
+
+    else:
+        print(" prints out the number as is")
+
+if __name__=="__main__":
+    main()
