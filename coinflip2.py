8361ad2cb8bf53e95702527e2489dd74b1edaafd
lidzytapia079-hub <lidzytapia079@gmail.com>
2026-09-02 20:16:28 +0000

    coinflip2

---
+++ coinflip2.py
@@ -0,0 +1,22 @@
+import random
+
+def main():
+    coin = ["heads", "tails"]
+    attempts = 3
+    while attempts >0:
+        flip = random.choice(coin)
+        guess= input("Heads or tails?").strip().lower()
+        print("The coin landed on", flip)
+        if guess==flip:
+            print("You won!")
+            break
+        else:
+            print("You lost")
+            attempts-=1
+            print("Attempts left:", attempts)
+
+
+
+if __name__=="__main__":
+    main()
+
