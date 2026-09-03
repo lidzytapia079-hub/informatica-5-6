20e3c76c022da7c35685af354dacb920b8cbf8ca
lidzytapia079-hub <lidzytapia079@gmail.com>
2026-09-03 20:15:53 +0000

    ch22

--- guess-number.py
+++ guess-number.py
@@ -1,8 +1,10 @@
 import random
 def main():
     name=input("Hello! What is your name? :" )
+    #f por q hay vavriables y texto en el mismo print
     print(f"Well,{name} I am thinking of a number between 1 and 100.")
-    guess = ""
+    #start with a random number
+    guess = 0
     number = random.randint(1,100)
 
     while guess !=number:
@@ -10,7 +12,7 @@ def main():
         guess = int(input("take a guess:"))
         if guess > number:
             print("Your guess is too high")
-            
+
         elif guess < number:
             print("Your guess is to low!")
 
