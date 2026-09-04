e9e21aaac11f32cd7df4ab53383fefee707dce81
lidzytapia079-hub <lidzytapia079@gmail.com>
2026-09-04 17:30:49 +0000

    learning2

---
+++ learning-app.py
@@ -0,0 +1,34 @@
+import random
+def main():
+     print("Welcome to sum app")
+
+    num1=random.randint(10,99)
+    num2=random.randint(10,99)
+    num3= num1+num2
+
+    correct=("Your answer are correct")
+    incorrect=("Your answer are wrong")
+    streak=""
+
+
+
+
+    while prompt !=result:
+        number1=random.randint(10,99)
+        number2=random.randint(10,99)
+        result:(number1 +number2)
+        print(f"What is {num1}+{num2}?")
+
+
+
+
+
+
+
+
+
+
+
+if __name__=="__main__":
+    main()
+
