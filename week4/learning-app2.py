3637869bc90e0cfcb94d466cf904fac60acc9dff
lidzytapia079-hub <lidzytapia079@gmail.com>
2026-09-09 20:03:12 +0000

    complete

---
+++ week4/learning-app2.py
@@ -0,0 +1,44 @@
+import random
+def main():
+    print("MATEMAKINA!") #matematicacitametam
+    star = "⭐"
+    streak = 0
+    a = ""
+    b = ""
+    op = ""
+    answer = ""
+
+    while streak != 3:
+        a = random.randint(10,99)
+        b = random.randint(10,99)
+        op = a + b
+
+        print(f"What is {a} + {b}?")
+        answer = int(input("Your answer: "))
+        if answer != op:
+            print("Incorrect.")
+            print(f"The answer was {op}")
+            if streak > 0:
+                streak -= streak
+        elif answer == op:
+            print("Correct!")
+            streak += 1
+            if streak == 1:
+                star == star
+            elif streak == 2:
+                star = "⭐⭐"
+            else:
+                star = "⭐⭐⭐"
+
+            print(f"Streak: {star}")
+
+        elif streak == 3:
+            break
+        else:
+            print("Invalid option.")
+
+
+
+
+if __name__ == "__main__":
+    main()
