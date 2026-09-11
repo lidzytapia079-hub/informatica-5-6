35e1bde210e27cfaecb8f989d88cd3832de5af70
lidzytapia079-hub <lidzytapia079@gmail.com>
2026-09-11 17:30:30 +0000

    list3

--- list-reference.py
+++ list-reference.py
@@ -3,6 +3,28 @@ def main():
     mylist=["pencil","computer","shirt","phone","paper"]
     print(len(mylist))
 
+    #.append()
+    fruits=["apple","orange","grapes"]
+    fruits.append("banana")
+    print(fruits)
+
+    #insert()
+    fruits1=["apple","orange","grape"]
+    fruits1.insert(2,"banana")
+    prints(fruits1)
+
+
+    #sort
+    numbers - [1, 4, 5, 7, 9, 3,]
+    numbers.sort()
+    print(numbers)
+
+    items - ["lettuce","tomato", "bread", "jam", "mayonaise"]
+    items.sort()
+    print(items)
+
+    numbers1=
+
 
 
 if __name__=="__main__":
