334ebef3f725b3c19c94c27ca47a5b10854ef902
lidzytapia079-hub <lidzytapia079@gmail.com>
2026-09-14 19:37:51 +0000

    radio-playlist

---
+++ radio-playlist.py
@@ -0,0 +1,38 @@
+from time import sleep
+def main():
+    playlist=["Boston","Dracula","I Knew It, I Knew You","hate that i made you love me","Risk It All"]
+    playlist.append("Be By You")
+    print(playlist)
+    playlist.insert(0,"Bohemian Rhapsody")
+    print(playlist)
+    playlist.pop(4)
+    print(playlist)
+    print(playlist.index("Risk It All"))
+    print("Number of songs in the playlist:", len(playlist))
+    playlist.reverse()#this method transforms the current list , it dosen´t creates a new one.
+    print(playlist)
+    playlist.sort
+    print(playlist)
+    repeat=10
+    while repeat>0:
+        print(playlist)
+        song_played = playlist[0]
+        playlist.pop(0)
+        playlist.append(song_played)
+        sleep(3)
+        repeat-=1
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__=="__main__":
+    main()
