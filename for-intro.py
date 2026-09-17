be07563393a96962899bf36b3ccad6aaf0cf5bbc
lidzytapia079-hub <lidzytapia079@gmail.com>
2026-09-17 17:19:43 +0000

    for1

---
+++ for-intro.py
@@ -0,0 +1,13 @@
+def main():
+
+    doctrines=["faith in him","repentence","baptisim","recive the holy ghost","endure until the end"]
+    for d in range(len(doctrines)):
+        print(doctrines[d])
+
+
+
+
+
+if __name__=="__main__":
+    main()
+
