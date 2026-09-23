6923f79826f1ac89006a3053290e43cfe0b549e7
lidzytapia079-hub <lidzytapia079@gmail.com>
2026-09-23 20:20:48 +0000

    tables

---
+++ tables.py
@@ -0,0 +1,22 @@
+def main():
+    number:["1","2","3","4","5","6","7","8","9","10"]
+    answer:[]
+    while True:
+        number=int(input("enter  a number between 1-10: "))
+        if number>=10:
+           for n in range(10):
+              print({number}*{[number]})
+
+        elif number
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
